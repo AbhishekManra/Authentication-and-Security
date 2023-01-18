d3ae5d8b72d9f5585b714714a598264ea905ce69
AbhishekManra <abhishek.manral2021@vitbhopal.ac.in>
2023-01-19 02:29:21 +0530

    Level4 - Using passport pacakge for authentication

--- app.js
+++ app.js
@@ -4,16 +4,27 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt"); 
-// we installed yet another package namely 'bcrypt' which will allow us to hash our passwords
-// We salt our password so that it will become more back-breaking for the any hacker.
-const saltrounds = 10;
-// salting mean adding a random hash to our orignal hash
-// bcrypt allow us to use salting rounds.
+// modules which we installed for our passport framework
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 app.set("view engine" , "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+
+// adding the session
+app.use(session({
+    secret: process.env.SECRET2,
+    saveUninitialized:false,
+    resave: false 
+}));
+
+app.use(passport.initialize()); // initalizing passport
+
+app.use(passport.session()); // launching the session
+
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/AuthDB2');
 
@@ -23,7 +34,15 @@ const userSchema = new mongoose.Schema({
     password : String
 }); 
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User" , userSchema); // our mongoose model alike which we will create new documents.
+
+passport.use(User.createStrategy()); // creating the strategy from our userschema
+
+passport.serializeUser(User.serializeUser()); // serialsation of the cookie
+passport.deserializeUser(User.deserializeUser());  // serialsation of the cookie
+
 
 app.get("/",function(req,res){
     res.render("home");
@@ -34,34 +53,43 @@ app.get("/login",function(req,res){
 app.get("/register",function(req,res){
     res.render("register");
 });
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/");
+    }
+});
 
 app.post("/register",function(req,res){
-    bcrypt.hash(req.body.password, saltrounds, function(err, hash) { // hash = it is our new hash key after 10 rounds of salting
-        const newuser = new User({
-            email : req.body.username,
-            password : hash 
-        });
-        newuser.save(function(err){
-            if(err){
-                console.log(err);
-            }else{
-                res.render("secrets");
-            }
-        });
+
+    User.register({username:req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+        }else{
+            //checking if there exists an existing entry in our db if no redirected to secrets page with a cookie stored in the store.
+            // and the new session created.
+            passport.authenticate('local')(req,res,function(){ 
+                res.redirect("/secrets");
+            });
+        }
     });
+
 });
 
 app.post("/login",function(req,res){
-    User.findOne({email : req.body.username},function(err,result){
+    
+    const newuser = new User({
+        username : req.body.username,
+        password : req.body.password
+    });
+    req.logIn(newuser,function(err){
         if(err){
             console.log(err);
         }else{
-            bcrypt.compare(req.body.password, result.password, function(err, result1) {
-               if(result1 === true){ 
-                res.render("secrets");
-               }else{
-                console.log(err);
-               }
+            //checking if there exists an existing entry in our db if yes then redirected to secrets page.
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
             });
         }
     });
